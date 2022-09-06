8a42d0d6979adb49ce5f17496b57a3e3a86d286e
Idel Lawrence Aranilla <106528291+idellolrence@users.noreply.github.com>
2022-09-06 15:27:25 +0800

    Create Main.java

---
+++ Exercise_3/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args){
+        
+        //Creating Stack Object
+        Stack burger = new Stack(5);
+        
+        System.out.println("Customized your burger by stacking!\n");
+        
+        //Customized your burger
+        burger.push("Bottom Bun");
+        burger.push("Beef Patty");
+        burger.push("Barbeque Sauce");
+        burger.push("Caramelized Onions");
+        burger.push("Top Bun");
+        
+        //How many layers does the burger has?
+        System.out.print("\nThe burger has " +burger.size());
+        System.out.println(" layers.");
+        
+        //What is the topmost layer of the burger?
+        System.out.println("\nThe topmost layer of the burger is the " +burger.peek());
+        
+        //Remove the topmost layer of the burger.
+        burger.pop();
+        
+        //Review your burger customization.
+        System.out.println("\nYour customized burger stack now is:");
+        burger.printStack();
+
+    }
+}
